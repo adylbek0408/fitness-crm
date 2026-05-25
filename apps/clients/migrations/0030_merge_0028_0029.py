90a31bc6752e7378deb2ba5e070d6b7aeb27bec1
adylbek0408 <adylbek0408@icloud.com>
2026-05-25 20:18:02 +0600

    clients: merge migration branches 0028 and 0029

---
+++ apps/clients/migrations/0030_merge_0028_0029.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('clients', '0028_enrollmentpayment_paid_at'),
+        ('clients', '0029_alter_client_status_add_active_frozen'),
+    ]
+
+    operations = []
